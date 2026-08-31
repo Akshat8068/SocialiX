068933b13abde3eac78d1d8eae9325d3b2aba7bb
Akshat <akshatyadav@Thoughtwin.com>
2026-08-31 16:55:48 +0530

    fix- in dev mode use nodemailer or in prod use resend

--- SocialiX-Backend/server/utils/mail.utils.ts
+++ SocialiX-Backend/server/utils/mail.utils.ts
@@ -1,41 +1,70 @@
+import nodemailer from "nodemailer";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY)
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD,
+    },
+});
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendOtpEmail = async (
     email: string,
     subject: string,
     otp: string
 ): Promise<void> => {
-    const { data, error } = await resend.emails.send({
-        from: "SocialiX <onboarding@resend.dev>",
-        to: ["akshatyadav@thoughtwin.com"],
-        subject,
-        html: `
-            <div style="font-family: Arial, sans-serif; max-width:600px; margin:auto;">
-                <h2>SocialiX</h2>
 
-                <p>Your One-Time Password (OTP) is:</p>
+    const html = `
+        <div style="font-family: Arial, sans-serif; max-width:600px; margin:auto;">
+            <h2>SocialiX</h2>
 
-                <h1 style="letter-spacing:5px; color:#2563eb;">
-                    ${otp}
-                </h1>
+            <p>Your One-Time Password (OTP) is:</p>
 
-                <p>This OTP is valid for <b>5 minutes</b>.</p>
+            <h1 style="letter-spacing:5px; color:#2563eb;">
+                ${otp}
+            </h1>
 
-                <p>If you did not request this OTP, please ignore this email.</p>
+            <p>This OTP is valid for <b>5 minutes</b>.</p>
 
-                <br/>
+            <p>If you did not request this OTP, please ignore this email.</p>
 
-                <p>Thanks,<br/>SocialiX Team</p>
-            </div>
-        `,
-    })
+            <br/>
 
-    if (error) {
-        console.error("Resend email error:", error)
-        throw new Error(error.message)
+            <p>Thanks,<br/>SocialiX Team</p>
+        </div>
+    `;
+
+    // dev
+    if (process.env.NODE_ENV === "development") {
+
+        await transporter.sendMail({
+            from: `"SocialiX" <${process.env.EMAIL_USER}>`,
+            to: email,
+            subject,
+            html,
+        });
+
+        console.log(`OTP sent using Nodemailer to ${email}`);
+        return;
     }
 
-    console.log("OTP email sent:", data)
+    // production
+    const { data, error } = await resend.emails.send({
+        from: "SocialiX <onboarding@resend.dev>",
+        to: [email],
+        subject,
+        html,
+    });
+
+    if (error) {
+        console.error( error);
+        throw new Error(error.message);
+    }
+
+    console.log("OTP sent using Resend:", data);
 };
